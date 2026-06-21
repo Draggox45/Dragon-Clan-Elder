7b74b53901faeb90c54e658567ad52a571d3e863
Draggox45 <draggox45@gmail.com>
2026-06-21 08:54:15 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Welcome to the Dragon Clan Elder Site</h1>
+      <p>Deployed directly from mobile!</p>
+    </div>
+  );
+}
